7798eebb38fd34b8e5c993c62508b56b7a116b16
Mateusz Kidawa <mateusz.kidawa@gmail.com>
2022-11-23 15:59:30 +0100

    zadanie domowe

--- src/components/GridList/GridList.component.tsx
+++ src/components/GridList/GridList.component.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import styles from './GridList.module.css'
 
-// interface GridListProps {
-//     children: React.ReactNode
-// }
-
-const GridList = ({children}: React.PropsWithChildren) => {
+const GridList = ({ children }: React.PropsWithChildren) => {
   return (
     <div className={styles.root}>
-        {children}
+      {children}
     </div>
   )
 }
